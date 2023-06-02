7860252628f62795224c8de161513f6dafcdc265
joaomarcos9741 <joaommarcosc@gmail.com>
2023-06-02 08:17:13 -0300

    Aprendendo mais um pouco

--- sctript.js
+++ sctript.js
@@ -2,14 +2,53 @@ const listelement = document.querySelector("#app ul")
 const InputElement = document.querySelector("#app input")
 const buttonElement = document.querySelector("#app button")
 
-const tarefas = ["Estudar para Prova", "Dormir cedo"]
+const tarefas = JSON.parse(localStorage.getItem("list")) || []
 
-for (const iterator of tarefas) {
-    const tarefaElement = document.createElement("li")
-    const tarefaTex = document.createTextNode(iterator)
 
-    tarefaElement.appendChild(tarefaTex)
-    listelement.appendChild(tarefaElement)
+function renderTarefas() {
+
+    listelement.innerHTML = ""
+
+    for (const iterator of tarefas) {
+        const tarefaElement = document.createElement("li")
+        const tarefaTex = document.createTextNode(iterator)
+
+        const linkElement = document.createElement("a")
+        linkElement.setAttribute("href", "#")
+
+        const pos = tarefas.indexOf(iterator)
+        linkElement.setAttribute("onclick", `deleteTarefas(${pos})`)
+
+        const linkText = document.createTextNode("Excluir")
+        linkElement.appendChild(linkText)
+
+
+        tarefaElement.appendChild(tarefaTex)
+        tarefaElement.appendChild(linkElement)
+        listelement.appendChild(tarefaElement)
+    }
+
+}
+renderTarefas()
+
+
+function addTarefas() {
+    const tarefaTex = InputElement.value
+    console.log(tarefaTex)
+    tarefas.push(tarefaTex)
+    InputElement.value = ""
+    renderTarefas()
+    saveToStorage()
+}
+buttonElement.onclick = addTarefas
+
+
+function deleteTarefas(pos){
+    tarefas.splice(pos, 1)
+    renderTarefas()
+    saveToStorage()
 }
 
-
+function saveToStorage(){
+    localStorage.setItem("list", JSON.stringify(tarefas))
+}
